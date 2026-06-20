7653b52761e002b0ed5b8fc7303379f9ad65d792
valdecimpj <valdecimpj@gmail.com>
2026-06-20 03:01:01 +0000

    delete build artifacts from source control

--- Módulo 3/ecommerce-webflux/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO produtos (nome, descricao, preco, estoque, ativo) VALUES
-    ('Notebook Pro 15', 'Notebook com processador i9 e 32GB RAM', 8999.90, 10, TRUE),
-    ('Mouse Gamer RGB',  'Mouse com 16000 DPI e iluminação RGB',    299.90, 50, TRUE),
-    ('Teclado Mecânico', 'Switches Cherry MX Red, ABNT2',           599.90, 30, TRUE),
-    ('Monitor 4K 27"',   'IPS, 144Hz, HDR400',                     3499.90,  8, TRUE),
-    ('Headset 7.1',      'Surround virtual, microfone removível',    449.90, 20, TRUE);
